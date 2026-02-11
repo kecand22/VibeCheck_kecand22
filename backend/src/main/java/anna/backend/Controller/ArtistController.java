9c0efc827da5a619b6e5643d0579056fce60da8f
kecand22 <kecand22@htl-kaindorf.at>
2026-02-11 17:16:46 +0100

    Create ArtistController

---
+++ backend/src/main/java/anna/backend/Controller/ArtistController.java
@@ -0,0 +1,18 @@
+package anna.backend.Controller;
+
+
+import anna.backend.Dto.ArtistDTO;
+import anna.backend.Services.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/artists")
+public class ArtistController {
+
+}
