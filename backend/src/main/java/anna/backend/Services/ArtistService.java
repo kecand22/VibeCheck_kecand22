28c4a97a570758a6d9ba74981fdea562ce4fbeb5
kecand22 <kecand22@htl-kaindorf.at>
2026-02-11 17:20:23 +0100

    Create ArtistService

---
+++ backend/src/main/java/anna/backend/Services/ArtistService.java
@@ -0,0 +1,15 @@
+package anna.backend.Services;
+
+import anna.backend.Dto.ArtistDTO;
+import anna.backend.Repositories.ArtistRepository;
+import anna.backend.entities.Artist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArtistService {
+
+}
